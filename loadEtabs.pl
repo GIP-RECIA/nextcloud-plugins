1dbeb07b7def09a06a2789cde6c3150693854420
Pierre Legay <pierre.legay@recia.fr>
2021-09-10 15:47:44 +0200

    LoadEtab debug last commit

--- loadEtabs.pl
+++ loadEtabs.pl
@@ -234,8 +234,8 @@ sub traitementEtab() {
 	} else {
 		$filtre = $filterGroup;
 		# ici $etab peut etre un nom de groupe (contenant des ()) on les vire.
-		$etab = s/\(/\\28/g;
-		 $etab = s/\)/\\29/g;
+		$etab =~ s/\(/\\28/g;
+		 $etab =~ s/\)/\\29/g;
 	}
 	my $disable = 0;
 	my $create = 0;
