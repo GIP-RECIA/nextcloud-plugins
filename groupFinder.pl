0941505b559d18034b8f570feacf88c5ff5a4eef
Pierre Legay <pierre.legay@recia.fr>
2021-09-13 12:19:42 +0200

    groupeFinder montre les partages en option

--- groupFinder.pl
+++ groupFinder.pl
@@ -11,24 +11,33 @@ binmode STDOUT, ':encoding(UTF-8)';
 # un script pour afficher/rechercher les utilisateurs d'un groupe.
 
 unless (@ARGV) {
-	print "usage :\t$0 [-m] groupePattern \n";
-	print "\t si -m on affiche les membres\n";
+	print "usage :\t$0 [-m][-p] groupePattern \n";
+	print "\t -m => affiche les membres\n";
+	print "\t -p => affiche les partage\n";
 	print "\t groupePattern est un pattern de groupe à la SQL (avec éventuelement des % ou _)\n"; 
 	exit 1;
 }
 
 my $withUser = 0;
+my $withPartage = 0;
 my $grpPattern;
-if ($ARGV[0] eq '-m' ) {
-	$withUser = 1;
-	$grpPattern = $ARGV[1];
-} else {
-	$grpPattern = $ARGV[0];
+foreach my $param (@ARGV) {
+	if ($param =~ /^-/) {
+		$withUser = 1 if $param eq '-m';
+		$withPartage = 1 if $param eq '-p';
+	} else {
+		$grpPattern = $param;
+	} 
+} 
+
+unless ($grpPattern) {
+	print "Error: manque le patterne de groupe\n";
+	exit 1;
 }
 
 my $sqlGroup = "select gid from  oc_groups where gid like ?";
 
-my $sqlMembre = "select u.uid, u.displayname, g.gid from oc_users u , oc_group_user g where g.gid = ? and g.uid = u.uid ";
+
 
 my $sql = connectSql();
 
@@ -43,16 +52,71 @@ while (my $tuple =  $sqlStatement->fetchrow_hashref()) {
 	push @allGroups, $gid;
 }
 
-$sqlStatement = $sql->prepare($sqlMembre) or die $sql->errstr;
+
 if (@allGroups) {
 	foreach my $gid (@allGroups) {
 		print "$gid : \n";
 		if ($withUser) {
-			$sqlStatement->execute($gid) or die $sqlStatement->errstr;
-			while (my $tuple =  $sqlStatement->fetchrow_hashref()) {
-				print "\t\t$tuple->{'uid'} $tuple->{displayname}\n";
-			}
+			&printMembers ($gid);
+		}
+		if ($withPartage) {
+			printPartage($gid);
 		}
 	}
 }
 print "\n", scalar @allGroups,  " groupes trouvés\n";
+
+
+sub printMembers {
+	my $gid = shift;
+	our $sqlMemberStatement;
+	unless ($sqlMemberStatement) {
+		my $sqlMembre = "select u.uid, u.displayname, g.gid from oc_users u , oc_group_user g where g.gid = ? and g.uid = u.uid ";
+		$sqlMemberStatement = $sql->prepare($sqlMembre) or die $sql->errstr;
+	}
+	
+	$sqlMemberStatement->execute($gid) or die $sqlMemberStatement->errstr;
+	while (my $tuple =  $sqlMemberStatement->fetchrow_hashref()) {
+		print "\t\t$tuple->{'uid'} $tuple->{displayname}\n";
+	}
+}
+
+sub printPartage {
+	my $gid = shift;
+	our $sqlPartageStatment;
+	unless ($sqlPartageStatment) {
+		my $sqlQuery = "select uid_owner, file_source, file_target, item_type, permissions from oc_share  where share_with = ? order by file_target, file_source";
+		$sqlPartageStatment = $sql->prepare($sqlQuery) or die $sql->errstr;
+	}
+	$sqlPartageStatment->execute($gid) or die $sqlStatement->errstr;
+	
+	while (my $tuple =  $sqlPartageStatment->fetchrow_hashref()) {
+		my $fileName = $tuple->{'file_target'};
+		my $fileId = $tuple->{'file_source'};
+		my $uidOwner = $tuple->{'uid_owner'};
+		my $type = $tuple->{'item_type'};
+		my $perm = &permissionDecode($tuple->{'permissions'});
+		print "\t$fileId : $type $fileName <- $uidOwner $perm\n";
+	}
+}
+sub permissionDecode {
+	my $perm = shift;
+	my $flags = "($perm";
+	
+	if ($perm < 0) {
+		return  "(permission possible:  Modification Création Supression Repartage)";
+	}
+	if ($perm & 2 ) {
+		$flags .= ' Mo'; # Modification
+	}
+	if ($perm & 4 ) {
+		$flags .= ' Cr'; # création
+	} 
+	if ($perm & 8 ) {
+		$flags .= ' Su'; # Supression
+	}
+	if ($perm & 16 ) {
+		$flags .= ' Re'; # Repartage
+	}
+	return $flags . ')';
+}
