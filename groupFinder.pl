85fa8bb5a0f1ea400422045398a2942c380378bf
Pierre Legay <pierre.legay@recia.fr>
2021-09-21 11:48:55 +0200

    groupFinder donne les uid bénéficiaires des partages

--- groupFinder.pl
+++ groupFinder.pl
@@ -72,32 +72,66 @@ sub printMembers {
 	my $gid = shift;
 	our $sqlMemberStatement;
 	unless ($sqlMemberStatement) {
-		my $sqlMembre = "select u.uid, u.displayname, g.gid from oc_users u , oc_group_user g where g.gid = ? and g.uid = u.uid ";
+		my $sqlMembre = "select u.uid, u.displayname, g.gid from oc_users u , oc_group_user g where g.gid = ? and g.uid = u.uid order by uid";
 		$sqlMemberStatement = $sql->prepare($sqlMembre) or die $sql->errstr;
 	}
 	
+	print "\tLes membres : ";
+	
 	$sqlMemberStatement->execute($gid) or die $sqlMemberStatement->errstr;
+	my $cpt = 0;
+	my $text ='';
 	while (my $tuple =  $sqlMemberStatement->fetchrow_hashref()) {
-		print "\t\t$tuple->{'uid'} $tuple->{displayname}\n";
+		 
+		my $tab;
+		
+		
+		unless ($cpt++ % 4) {
+			$tab = "\n\t\t";
+		} else {
+			$tab = (" " x int((40-length($text))));
+		}
+		
+		$text = "$tuple->{'uid'} $tuple->{displayname}";
+		print "$tab$text";
 	}
+	print "\n";
 }
 
 sub printPartage {
 	my $gid = shift;
 	our $sqlPartageStatment;
+	our $sqlPartagesUserStatement;
 	unless ($sqlPartageStatment) {
-		my $sqlQuery = "select uid_owner, file_source, file_target, item_type, permissions from oc_share  where share_with = ? order by file_target, file_source";
+		my $sqlQuery = "select uid_owner, file_source, path, item_type, permissions , id from recia_share  where share_with = ? order by path , uid_owner";
 		$sqlPartageStatment = $sql->prepare($sqlQuery) or die $sql->errstr;
+		$sqlQuery = "select share_with,  permissions from oc_share where file_source = ? and share_type = 2 and parent = ? order by share_with";
+		$sqlPartagesUserStatement =  $sql->prepare($sqlQuery) or die $sql->errstr;
 	}
 	$sqlPartageStatment->execute($gid) or die $sqlStatement->errstr;
+	print "\t les partages :\n" ;
 	
-	while (my $tuple =  $sqlPartageStatment->fetchrow_hashref()) {
-		my $fileName = $tuple->{'file_target'};
-		my $fileId = $tuple->{'file_source'};
-		my $uidOwner = $tuple->{'uid_owner'};
-		my $type = $tuple->{'item_type'};
-		my $perm = &permissionDecode($tuple->{'permissions'});
-		print "\t$fileId : $type $fileName <- $uidOwner $perm\n";
+	while (my $partageFile =  $sqlPartageStatment->fetchrow_hashref()) {
+		my $fileName = $partageFile->{'path'};
+		my $fileId = $partageFile->{'file_source'};
+		my $uidOwner = $partageFile->{'uid_owner'};
+		my $type = $partageFile->{'item_type'};
+		my $perm = &permissionDecode($partageFile->{'permissions'});
+		print "\t\t$fileId : $type $fileName <- $uidOwner $perm";
+		
+		$sqlPartagesUserStatement->execute($fileId , $partageFile->{'id'}) or die $sqlPartagesUserStatement->errstr; 
+		my $cpt = 0;
+		
+		while (my $partageUser =  $sqlPartagesUserStatement->fetchrow_hashref()) {
+			my $uid = $partageUser->{'share_with'};
+			my $perm = &permissionDecode($partageUser->{'permissions'});
+			unless ($cpt++ % 5 ) {
+				print "\n\t\t--> $uid $perm";
+			} else {
+				print ",\t\t$uid $perm";
+			}
+		}
+		print "\n";
 	}
 }
 sub permissionDecode {
