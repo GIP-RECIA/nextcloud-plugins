4fc94e132ba1ab9fd7ef33c7eb0e1eac9053743a
gbrousse-recia <gregory.brousse@recia.fr>
2022-01-06 09:49:11 +0100

    Fix : update js func to scroll on click

--- cssjsloader/inputs/js/fix-share-menu.js
+++ cssjsloader/inputs/js/fix-share-menu.js
@@ -1,2 +1,7 @@
 /* scroll to top when menu button is clicked */
-document.querySelector('#tab-sharing .action-item__menutoggle').onclick=()=>{window.scrollTo(0,0);}
+document.addEventListener('click',function(e){
+    if(e.target == document.querySelector('#tab-sharing .action-item__menutoggle')){
+        console.debug('[Debug] cssjsloader : click menu button')
+        window.scrollTo(0,0);
+    }
+});
