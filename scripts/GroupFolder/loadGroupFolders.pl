220b47df96f01b47c41b69f37697546d4467397d
Pierre Legay <pierre.legay@recia.fr>
2023-03-15 08:33:58 +0100

    use mylogger

--- scripts/GroupFolder/loadGroupFolders.pl
+++ scripts/GroupFolder/loadGroupFolders.pl
@@ -3,5 +3,24 @@ use strict;
 use FindBin;
 use lib $FindBin::Bin;
 use DBI();
-use Net::LDAP;
+use Net::LDAP; #libnet-ldap-perl
 use util;
+use YAML::XS 'LoadFile'; #libyaml-libyaml-perl
+use Data::Dumper;
+use MyLogger;
+
+MyLogger::level(4)
+;my $config = LoadFile($FindBin::Bin.'/config.yml'); #$yaml->[0];
+print "l'entrée: ", Dumper($config);
+
+
+my $RegexGroup;
+
+#my $sql = connectSql();
+
+my $ldap = connectLdap();
+
+foreach my $etab (@$config) {
+	$RegexGroup = $etab->{groups};
+	print Dumper($RegexGroup);
+}
