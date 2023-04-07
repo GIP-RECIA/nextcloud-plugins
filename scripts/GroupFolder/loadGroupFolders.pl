7341956346fb316b73833b8e82809c4c273afcbf
Pierre Legay <pierre.legay@recia.fr>
2023-04-07 14:17:28 +0200

    GroupFolder.pm gestion des Groups et des Folders

--- scripts/GroupFolder/loadGroupFolders.pl
+++ scripts/GroupFolder/loadGroupFolders.pl
@@ -37,6 +37,7 @@ use MyLogger;
 use Folder;
 use Group;
 use Etab;
+use GroupFolder;
 
 MyLogger::level(5, 1);
 
@@ -155,45 +156,22 @@ END {
 sub traitementRegexGroup {
 	my $etabNC = shift;
 	my $regexGroupList = shift;
-	my @res = @_;
+	my @grpRegexMatches = @_;
 	
 	# TRACE! Dumper(@res);
 	foreach my $regexGroup (@{$regexGroupList}) {
 		my $groupFormat = $regexGroup->{group};
-		my $folderFormat = $regexGroup->{folder};
-		my $adminFormat = $regexGroup->{admin};
-		my $quotaF = $regexGroup->{quotaF};
-		my $permF = $regexGroup->{permF};
-		
+
 		if ($groupFormat) {
-			my $group = Group->getOrCreateGroup(sprintf($groupFormat, @res), $etabNC);
-
-			DEBUG! "\t\t" , Dumper($group);
-
-			if ($folderFormat) {
-				my $folder = Folder->updateOrCreateFolder(sprintf($folderFormat, @res), $quotaF);
-				if ($folder) {
-					$folder->addGroup($group, @$permF);
-					DEBUG! "\t\t\tgroup folder ", Dumper($folder);
-				}
-			}
-
-			if ($adminFormat) {
-				my $folderAdmin = sprintf($adminFormat, @res);
-				DEBUG! "\t\t\tgroup folder admin: ",  $folderAdmin;
-				my $folder = Folder->getFolder($folderAdmin);
-				if ($folder) {
-					DEBUG! "\t\t\t\tgroup folder admin add group";
-					$folder->addAdminGroup($group);
-				} else {
-					if (index($folderAdmin, '^') == 0 ) {
-						my @folderList = Folder->findFolders($folderAdmin);
-						foreach my $f (@folderList) {
-							$f->addAdminGroup($group);
-						}
-					} 
-				}
-			}
+			GroupFolder->createGroupAndFolder(
+					$etabNC,
+					$groupFormat,
+					$regexGroup->{folder},
+					$regexGroup->{admin},
+					$regexGroup->{quotaF},
+					$regexGroup->{permF},
+					@grpRegexMatches
+				);
 		}
 	}
 }
@@ -223,7 +201,6 @@ sub traitementEtab {
 	if ($lastTimeStampLdap) {
 		$filtreLdap = sprintf( "(&%s(modifytimestamp>=%sZ))", $filtreLdap, $lastTimeStampLdap);
 	}
-	
 	
 	INFO! "filtre ldap =", $filtreLdap;
 	my @ldapGroups = util->searchLDAP('ou=groups', $filtreLdap, 'cn');
