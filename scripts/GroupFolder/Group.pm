090fb73901db41d723f0cc32f79aa4b06bef2df8
Pierre Legay <pierre.legay@recia.fr>
2023-04-17 17:08:39 +0200

    Group: Mise à Jour des DisplayNames et de l'apartenance aux étabs

--- scripts/GroupFolder/Group.pm
+++ scripts/GroupFolder/Group.pm
@@ -56,36 +56,39 @@ sub getOrCreateGroup {
 	
 	my $gid = NFKD($name . ':LDAP');
 	$gid =~ s/\p{NonspacingMark}//g; #suppression des accents
+
+	my $inEtab = 0;
 	
 	my $group = $etab->groupsNC->{$gid};
-	if  ($group) {
-		return $group;
+	if ($group) {
+		$inEtab = 1;
+	} else {
+		$group = $groupInBase{$gid};
+		unless ($group) {
+			$group = $groupInBase{$name};
+		}
 	}
 
-	$group = $etab->groupsNC->{$name};
-	if  ($group) {
-		return $group;
+	if ($group) {
+		if ($group->name() ne $name) {
+			util->executeSql(q/update IGNORE oc_groups set displayname = ? where gid = ?/, $name, $gid);
+			$group->name($name);
+		}
 	}
-
-	$group = $groupInBase{$gid};
 	
-	unless ($group) { 
-		$group = $groupInBase{$name};
-	}
-
 	unless ($group) {
 		$group = Group->new($gid, $name);
 		util->occ("group:add --display-name '$name' '$gid'");
 		$groupInBase{$gid} = $group;
-
-			# les groupes ne sont ajoutés à l'etab qu'a leurs creations du coup il ne peuvent pas être partager entre etab 
-		if ($etab) {
-			# le ignore dans le cas ou le group prexistait (l'insert termine alors normalement).
-			util->executeSql(q/insert IGNORE into oc_asso_uai_user_group (id_etablissement, user_group) values (?, ?)/, $etab->idBase, $gid);
-			$etab->groupsNC->{$gid} = $group;
-		}
 		DEBUG! "new group " , Dumper($group);
 	}
+
+	if ($etab && !$inEtab) {
+			# le ignore dans le cas ou le group prexistait (l'insert termine alors normalement).
+		util->executeSql(q/insert IGNORE into oc_asso_uai_user_group (id_etablissement, user_group) values (?, ?)/, $etab->idBase, $gid);
+		$etab->groupsNC->{$gid} = $group;
+	}
+
 	return $group;
 }
 
