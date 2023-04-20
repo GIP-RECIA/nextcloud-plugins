6c071371a73af0cd6e7d13526f6960e0488afb74
Pierre Legay <pierre.legay@recia.fr>
2023-04-20 14:57:32 +0200

    zip log file

--- scripts/launchLoadEtab.sh
+++ scripts/launchLoadEtab.sh
@@ -4,7 +4,7 @@ fdata=$HOME/data
 rcode=$HOME/scripts
 
 find $rlog \( -name '*.log' -o -name '*.log.*gz' \) -a -ctime +7 -delete
-
+find $rlog  -name 'groupFolders.*.log' -exec gzip  \{\} \;
 
 /bin/tar -czf $rlog/Loader/loadEtab.log.`date +'%d.%Hh'`.tgz $rlog/Loader/*.log.gz $fdata/allEtab.*
 
