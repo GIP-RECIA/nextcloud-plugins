d72cf1925075aa7539cc1bf63e71e23d0a9a5679
Pierre Legay <pierre.legay@recia.fr>
2023-05-30 09:39:45 +0200

    scipt wrapper  mail

--- scripts/mail.pl
+++ scripts/mail.pl
@@ -9,18 +9,20 @@ my $isOpen;
 
 
 while (<>) {
-	unless ($isOpen) {
-		if (/^\s*$/) {
-			next;
-		}
-		open (MAIL, "| /usr/bin/mail -s '$subject' -r '$from' ". 'ent@recia.fr') or die $!;
-		$isOpen = 1;
+	if (/^\s*$/) {
+		next;
 	}
+	open (MAIL, "| /usr/bin/mail -s '$subject' -r '$from' ". 'ent@recia.fr') or die $!;
+	$isOpen = 1;
 	print MAIL $_;
 }
+
 if ($isOpen) {
+	while (<>) {print MAIL $_ ;}
 	close MAIL;
 }
 
 
 __END__
+/var/www/ncgip.recia/scripts/mail.pl 'cron loadGroupFolder' 'ncgip@aquaray.com'
+/var/www/ncprod.recia/scripts/mail.pl 'cron loadGroupFolder' 'ncprod@aquaray.com'
