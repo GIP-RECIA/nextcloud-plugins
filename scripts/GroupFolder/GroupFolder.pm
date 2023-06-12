26afd8380197e12009d30c02b28e191168088ede
Pierre Legay <pierre.legay@recia.fr>
2023-06-12 09:38:34 +0200

    1 groupe peut être associé a plusieurs folders

--- scripts/GroupFolder/GroupFolder.pm
+++ scripts/GroupFolder/GroupFolder.pm
@@ -6,27 +6,12 @@ use strict;
 use utf8;
 use Data::Dumper;
 
-sub createGroupAndFolder {
+sub addGroup4AdminFolder {
 	my $class = shift;
 	my $etabNC = shift;
-	my $groupFormat = shift;
-	my $folderFormat = shift;
+	my $groupNC = shift;
 	my $adminFormat = shift;
-	my $quotaF = shift;
-	my $permF = shift;
 	my @grpMatched = @_;
-
-	my $group = Group->getOrCreateGroup(sprintf($groupFormat, @grpMatched), $etabNC);
-
-	DEBUG! "\t\t" , Dumper($group);
-
-	if ($folderFormat) {
-		my $folder = Folder->updateOrCreateFolder(sprintf($folderFormat, @grpMatched), $quotaF);
-		if ($folder) {
-			$folder->addGroup($group, @$permF);
-			DEBUG! "\t\t\tgroup folder ", Dumper($folder);
-		}
-	}
 
 	if ($adminFormat) {
 		my $folderAdmin = sprintf($adminFormat, @grpMatched);
@@ -34,16 +19,37 @@ sub createGroupAndFolder {
 		if (index($folderAdmin, '^') == 0 ) {
 			my @folderList = Folder->findFolders($folderAdmin);
 				foreach my $f (@folderList) {
-					$f->addAdminGroup($group);
+					$f->addAdminGroup($groupNC);
 				}
 		} else {
 			my $folder = Folder->getFolder($folderAdmin);
 			if ($folder) {
 				DEBUG! "\t\t\t\tgroup folder admin add group";
-				$folder->addAdminGroup($group);
+				$folder->addAdminGroup($groupNC);
 			}
 		} 
 	}
+}
+
+sub createFolder4Group {
+	my $class = shift;
+	my $etabNC = shift;
+	my $folderFormat = shift;
+	my $quotaF = shift;
+	my $permF = shift;
+	my $groupNC = shift;
+	my @grpMatched = @_;
+
+	if ($groupNC) {
+		if ($folderFormat) {
+			my $folder = Folder->updateOrCreateFolder(sprintf($folderFormat, @grpMatched), $quotaF);
+			if ($folder) {
+				$folder->addGroup($groupNC, @$permF);
+				DEBUG! "\t\t\tgroup folder ", Dumper($folder);
+			}
+		}
+	}
+	
 }
 
 1;
