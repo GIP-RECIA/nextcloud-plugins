6a869e57c4775021b3864a0f54f517384234472b
gbrousse-recia <gregory.brousse@recia.fr>
2023-06-16 16:44:48 +0200

    fix: update group

--- groupfolders/l10n/ps.js
+++
@@ -1,8 +0,0 @@
-OC.L10N.register(
-    "groupfolders",
-    {
-    "Delete" : "ړنګول",
-    "Share" : "شریکول",
-    "Groups" : "ګروپونه"
-},
-"nplurals=2; plural=(n != 1);");
--- groupfolders/l10n/ur_PK.js
+++
@@ -1,8 +0,0 @@
-OC.L10N.register(
-    "groupfolders",
-    {
-    "Delete" : "حذف کریں",
-    "Share" : "تقسیم",
-    "Group" : "Group"
-},
-"nplurals=2; plural=(n != 1);");
