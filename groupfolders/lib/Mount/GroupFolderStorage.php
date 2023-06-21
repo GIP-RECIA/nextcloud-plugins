1de96ec27f81dad1af9c29c96a97b3830e4e73d4
gbrousse-recia <gregory.brousse@recia.fr>
2023-06-21 14:29:38 +0200

    fix : groupfolder patch new version

---
+++ groupfolders/lib/Mount/GroupFolderStorage.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * @copyright Copyright (c) 2018 Robin Appelman <robin@icewind.nl>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\GroupFolders\Mount;
+
+use OC\Files\Cache\Scanner;
+use OC\Files\ObjectStore\NoopScanner;
+use OC\Files\ObjectStore\ObjectStoreStorage;
+use OC\Files\Storage\Wrapper\Quota;
+use OCP\Files\Cache\ICacheEntry;
+use OCP\Files\Storage\IDisableEncryptionStorage;
+use OCP\IUser;
+use OCP\IUserSession;
+
+class GroupFolderStorage extends Quota {
+	private int $folderId;
+	private ICacheEntry $rootEntry;
+	private IUserSession $userSession;
+	private ?IUser $mountOwner = null;
+	/** @var RootEntryCache|null */
+	public $cache = null;
+
+	public function __construct($parameters) {
+		parent::__construct($parameters);
+		$this->folderId = $parameters['folder_id'];
+		$this->rootEntry = $parameters['rootCacheEntry'];
+		$this->userSession = $parameters['userSession'];
+		$this->mountOwner = $parameters['mountOwner'];
+	}
+
+	public function getFolderId(): int {
+		return $this->folderId;
+	}
+
+	public function getOwner($path) {
+		$user = $this->userSession->getUser();
+		if ($user !== null) {
+			return $user->getUID();
+		}
+		return $this->mountOwner !== null ? $this->mountOwner->getUID() : false;
+	}
+
+	public function getCache($path = '', $storage = null) {
+		if ($this->cache) {
+			return $this->cache;
+		}
+		if (!$storage) {
+			$storage = $this;
+		}
+
+		$this->cache = new RootEntryCache(parent::getCache($path, $storage), $this->rootEntry);
+		return $this->cache;
+	}
+
+	/**
+	 * get a propagator instance for the cache
+	 *
+	 * @param \OC\Files\Storage\Storage (optional) the storage to pass to the watcher
+	 * @return \OC\Files\Cache\Propagator
+	 */
+	public function getPropagator($storage = null) {
+		if (!$storage) {
+			$storage = $this;
+		}
+		if (!isset($this->propagator)) {
+			$this->propagator = new GroupFolderPropagator($storage, \OC::$server->getDatabaseConnection(), \OC::$server->getLogger());
+		}
+		return $this->propagator;
+	}
+
+	public function getScanner($path = '', $storage = null) {
+		/** @var \OC\Files\Storage\Storage $storage */
+		if (!$storage) {
+			$storage = $this;
+		}
+		if ($storage->instanceOfStorage(ObjectStoreStorage::class)) {
+			$storage->scanner = new NoopScanner($storage);
+		} elseif (!isset($storage->scanner)) {
+			$storage->scanner = new Scanner($storage);
+		}
+		return $storage->scanner;
+	}
+}
