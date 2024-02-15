7daced48d13e50979a9e1f4f9b5bfd84722554f7
Pierre Legay <pierre.legay@recia.fr>
2024-02-15 09:43:17 +0100

    Nettoyage de code

--- scripts/checkOO.pl
+++ scripts/checkOO.pl
@@ -1,15 +1,10 @@
 #!/usr/bin/perl
 use strict;
 use utf8;
-use DBI();
-# les 2 use suivant permette de trouver les libraries installées
-#  dans le meme path que l'executable
 use FindBin; 			# ou est mon executable
 use lib "$FindBin::Bin/GroupFolder"; 	# chercher les lib au meme endroit
 use util;
 binmode STDOUT, ':encoding(UTF-8)';
-package MyLogger;
-
 
 my $sql = util->connectSql();
 
@@ -19,6 +14,6 @@ if (@row_ary) {
 	my $erreur = $row_ary[0];
 	if ($erreur) {
 		print $erreur, "\n";
-		util->occ("onlyoffice:documentserver --check", sub {print $_, "\n"}) ;
+		util->occ("onlyoffice:documentserver --check", sub {print $_;}) ;
 	}
 }
