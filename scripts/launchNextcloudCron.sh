69acfaea9cb5bba071ff3e546438087c594b89dd
Pierre Legay <pierre.legay@recia.fr>
2024-09-11 15:02:40 +0200

    cron : split en 3 fichiers de log

--- scripts/launchNextcloudCron.sh
+++ scripts/launchNextcloudCron.sh
@@ -2,12 +2,17 @@
 day=`date +%d`
 path=$HOME/logs-esco/NCcron
 
+
 log=$path/$day.log
 
 lock=$path/lock
 
-date '+%F %T' >> $log
-(flock -n 7 || flock -n 8 || flock -n 9 || exit 1;
-        php  -f $HOME/web/cron.php 2>&1 | tee -a  $log 
+lck=7
+
+(flock -n $lck || flock -n $((++lck)) || flock -n $((++lck)) || exit 1;
+	log=$path/$day.$lck.log
+	date '+%F %T' >> $log
+        php  -f $HOME/web/cron.php 2>&1 | tee -a  $log
+    date '+%T' >> $log
+
 ) 7>${lock}7 8>${lock}8 9>${lock}9
-date '+%T' >> $log
