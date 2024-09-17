26d5eed30c4c6b64a1a7ea8a829444de388407d4
Pierre Legay <pierre.legay@recia.fr>
2024-09-17 17:06:05 +0200

    use s3 info commande instead ls

--- scripts/GroupFolder/Folder.pm
+++ scripts/GroupFolder/Folder.pm
@@ -283,7 +283,7 @@ sub diffBaseDisque {
 	}
 	
 	if ($isStockageObject) {
-		my $s3command = util->lsCommande(util->getBucketName());
+		my $s3command = util->infoCommande(util->getBucketName());
 		my %pathInObject;
 		while (my ($path, $fileid) = each %pathInBase) {
 			§SYSTEM "$s3command/$fileid", OUT => sub { $pathInObject{$path} = $fileid if /\:$fileid\ \(object\):$/ };
