2f3de4f7b7e38e072cbeae67c1635700db276122
Pierre Legay <pierre.legay@recia.fr>
2024-09-18 08:48:25 +0200

    script removeOldUser sans info dans le mail

--- scripts/launchRemoveUser.sh
+++ scripts/launchRemoveUser.sh
@@ -8,6 +8,6 @@ rcode=$HOME/scripts
 /bin/gzip $rlog/removeOldUser/*.log 
 
 echo "Suppression définitive des comptes obsolètes: "
-/usr/bin/nice $rcode/removeOldUser.pl -n 2000 -l 4 2>&1
+/usr/bin/nice $rcode/removeOldUser.pl -n 2000 -l 41 2>&1
 
 date
