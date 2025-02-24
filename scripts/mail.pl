1548fc129a7fb78fe5071eb3f2e9bd32cac865d0
Pierre Legay <pierre.legay@recia.fr>
2025-02-24 10:43:23 +0100

    Script: Suppression de  libpng warning des mails

--- scripts/mail.pl
+++ scripts/mail.pl
@@ -6,14 +6,17 @@ my $subject=shift;
 my $from =shift;
 
 while (<>) {
+	next if /^libpng warning/;
 	unless (/^\s*$/) {
 		open (MAIL, "| /usr/bin/mail -s '$subject' -r '$from' ". 'ent@recia.fr') or die $!;
 		do {
+			next if /^libpng warning/;
 			print MAIL ;
 		} while <>;
 		close MAIL;
 		last;
 	}
 }
+
 
 __END__
