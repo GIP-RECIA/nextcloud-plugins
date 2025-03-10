56a699a38a218da751670b43a26acd7c1aff56b0
Pierre Legay <pierre.legay@recia.fr>
2025-03-10 09:09:53 +0100

    Suppression de warning dans le mail

--- scripts/mail.pl
+++ scripts/mail.pl
@@ -6,9 +6,11 @@ my $subject=shift;
 my $from =shift;
 
 while (<>) {
+	next if /^libpng warning/;
 	unless (/^\s*$/) {
 		open (MAIL, "| /usr/bin/mail -s '$subject' -r '$from' ". 'ent@recia.fr') or die $!;
 		do {
+			next if /^libpng warning/;
 			print MAIL ;
 		} while <>;
 		close MAIL;
