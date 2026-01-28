bfb174a700aa2d092632545cb6818d689c8d4d3f
Quentin Guillemin <quentin.guillemin@recia.fr>
2026-01-28 14:03:14 +0100

    chore: migrate Nextcloud from v31.0.9 to v31.0.13

--- lib/private/Files/ObjectStore/ReciaObjectStoreStorage.php
+++ lib/private/Files/ObjectStore/ReciaObjectStoreStorage.php
@@ -1,32 +1,14 @@
 <?php
+
 /**
- * @copyright Copyright (c) 2016, ownCloud, Inc.
- *
- * @author Björn Schießle <bjoern@schiessle.org>
- * @author Jörn Friedrich Dreyer <jfd@butonic.de>
- * @author Morris Jobke <hey@morrisjobke.de>
- * @author Roeland Jago Douma <roeland@famdouma.nl>
- *
- * @license AGPL-3.0
- *
- * This code is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License, version 3,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License, version 3,
- * along with this program. If not, see <http://www.gnu.org/licenses/>
- *
+ * SPDX-FileCopyrightText: 2016-2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-FileCopyrightText: 2016 ownCloud, Inc.
+ * SPDX-License-Identifier: AGPL-3.0-only
  */
 
 namespace OC\Files\ObjectStore;
 
-
-class ReciaObjectStoreStorage extends ObjectStoreStorage { 
+class ReciaObjectStoreStorage extends ObjectStoreStorage {
 
 	protected $defautBucket;
 	protected $defautId;
@@ -39,63 +21,43 @@ class ReciaObjectStoreStorage extends ObjectStoreStorage {
 	 */
 	public function __construct($params) {
 		parent::__construct($params);
-		
+
 		$this->defautObjectStore = $params['objectstore'];
-		$this->reciaObjectStore = new  S3Recia($params);
+		$this->reciaObjectStore = new S3Recia($params);
 		$this->defautBucket = $this->defautObjectStore->getBucket();
 		$this->defautId = $this->defautObjectStore->getStorageId();
-  
- 	}
-
+	}
 
 	public function stat(string $path): array|false {
-#	try {
+		// try {
 		$stat = parent::stat($path);
-		if (!(is_null($this->objectStore) or is_null($this->defautObjectStore) )) {
-
-			if  (preg_match("/^[^\/]+\/avatar/",$path,  $matches)) {
-				if (preg_match("/avatar\/(F\w{7})(\/.*)?$/",$path,  $matches)) {
+		if (!(is_null($this->objectStore) or is_null($this->defautObjectStore))) {
+			if (preg_match("/^[^\/]+\/avatar/", $path,  $matches)) {
+				if (preg_match("/avatar\/(F\w{7})(\/.*)?$/", $path,  $matches)) {
 					$uid = $matches[1];
 					if (! is_null($this->reciaObjectStore)) {
 						$this->objectStore = $this->reciaObjectStore;
 						$this->objectStore->setBucket($this->defautBucket . strtolower($uid));
-
 					}
 				}
-			} elseif  (preg_match("/^appdata_[^\/]+\/preview\/(\d+)/",$path,  $matches)) {
+			} elseif (preg_match("/^appdata_[^\/]+\/preview\/(\d+)/", $path,  $matches)) {
 				$rep = $matches[1];
 				if (! is_null($this->reciaObjectStore)) {
 					$num = $rep % 1000;
 					$this->objectStore = $this->reciaObjectStore;
-					$bucketName = $this->defautBucket .'preview'. $num;
+					$bucketName = $this->defautBucket . 'preview' . $num;
 					$this->objectStore->setBucket($bucketName);
-					#$this->objectStore = $this->defautObjectStore ;
-					#error_log(" bucket name = $bucketName \n " , 3, '/var/www/ncrecette.recia/logs-esco/object.log'); 
+					// $this->objectStore = $this->defautObjectStore;
+					// error_log(" bucket name = $bucketName \n " , 3, '/var/www/ncrecette.recia/logs-esco/object.log'); 
 				}
- 			} else {
-				$this->objectStore = $this->defautObjectStore ;
+			} else {
+				$this->objectStore = $this->defautObjectStore;
 			}
 		}
-#	} catch (Exception $e) {
-#		error_log(" Execption $e->gerMessage() \n " , 3, '/var/www/ncrecette.recia/logs-esco/object.log');
-#	}
+		// } catch (Exception $e) {
+		// 	error_log(" Execption $e->gerMessage() \n " , 3, '/var/www/ncrecette.recia/logs-esco/object.log');
+		// }
+
 		return $stat;
 	}
-	
- /**
-  * Override this method if you need a different unique resource identifier for your object storage implementation.
-  * The default implementations just appends the fileId to 'urn:oid:'. Make sure the URN is unique over all users.
-  * You may need a mapping table to store your URN if it cannot be generated from the fileid.
-  *
-  * @param int $fileId the fileid
-  * @return null|string the unified resource name used to identify the object
-  
- public function getURN($fileId) {
-         if (is_numeric($fileId)) {
-                 return $this->objectPrefix . $fileId;
-         }
-    #     error_log(" bucket name = $bucketName \n " , 3, '/var/www/ncrecette.recia/logs-esco/object.log'); 
-         return null;
- }
-*/
 }
