6570a804dc74e36f483d57562c9f23b200bf6066
Quentin Guillemin <quentin.guillemin@recia.fr>
2026-02-13 10:35:30 +0100

    feat(files_sharing): migrate Nextcloud from v31.0.13 to v31.0.14

--- files_sharing/app/composer/composer/installed.php
+++ files_sharing/app/composer/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '31.0.13.x-dev',
-        'version' => '31.0.13.9999999-dev',
-        'reference' => 'c9e5c695a79100c23beb5444f1a4825410195fd9',
+        'pretty_version' => '31.0.14.x-dev',
+        'version' => '31.0.14.9999999-dev',
+        'reference' => 'aec70613fe00fb9bbeec2c5e6726fc3353333ce2',
         'type' => 'library',
         'install_path' => __DIR__ . '/../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '31.0.13.x-dev',
-            'version' => '31.0.13.9999999-dev',
-            'reference' => 'c9e5c695a79100c23beb5444f1a4825410195fd9',
+            'pretty_version' => '31.0.14.x-dev',
+            'version' => '31.0.14.9999999-dev',
+            'reference' => 'aec70613fe00fb9bbeec2c5e6726fc3353333ce2',
             'type' => 'library',
             'install_path' => __DIR__ . '/../',
             'aliases' => array(),
